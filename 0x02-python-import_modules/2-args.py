fd1f6b4a8025add229b1d9b921c4c7680715e136
sabaghhh <22-101257@students.eui.edu.eg>
2024-01-06 01:27:37 +0200

    adjusting files

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,18 +1,19 @@
 #!/usr/bin/python3
-if __name__ == "__main__":
-    import sys
-    i = len(sys.argv) - 1
+import sys
 
-    if i == 0:
-        print("{} arguments.".format(i))
-    elif i == 1:
-        print("{} argument:".format(i))
+
+def main():
+    n = len(sys.argv)
+    if n <= 1:
+        print("0 arguments.")
     else:
-        print("{} arguments:".format(i))
+        if n == 2:
+            print("{} argument:".format(n-1))
+        else:
+            print("{} arguments:".format(n-1))
+        for i in range(1, n):
+            print("{}: {}".format(i, sys.argv[i]))
 
-    if i >= 1:
-        i = 0
-        for arg in sys.argv:
-            if i != 0:
-                print("{}: {}".format(i, arg))
-            i += 1
+
+if __name__ == "__main__":
+    main()
