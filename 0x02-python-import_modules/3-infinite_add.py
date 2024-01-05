e248dbfae34a0697cab19c086d2bf00a7b56fdb5
sabaghhh <22-101257@students.eui.edu.eg>
2024-01-06 01:24:07 +0200

    adjusting files

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,17 +1,8 @@
 #!/usr/bin/python3
-import sys
-
-
-def main():
-    n = len(sys.argv)
-    num = 0
-    if n <= 1:
-        print(0)
-    else:
-        for i in range(1, n):
-            num = int(sys.argv[i]) + num
-        print(num)
-
-
 if __name__ == "__main__":
-    main()
+    import sys
+    result = 0
+    for arg in sys.argv:
+        if arg != sys.argv[0]:
+            result += int(arg)
+    print(result)
