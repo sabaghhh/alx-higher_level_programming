1c12b87fa33072b620b02dd00b2c75ec1e9e24e4
sabaghhh <22-101257@students.eui.edu.eg>
2024-01-30 01:17:06 +0200

    adjusting files

--- 0x05-python-exceptions/0-main.py
+++ 0x05-python-exceptions/0-main.py
@@ -1,11 +1,6 @@
 #!/usr/bin/python3
-safe_print_list = __import__('0-safe_print_list').safe_print_list
+Square = __import__('0-square').Square
 
-my_list = [1, 2, 3, 4, 5]
-
-nb_print = safe_print_list(my_list, 2)
-print("nb_print: {:d}".format(nb_print))
-nb_print = safe_print_list(my_list, len(my_list))
-print("nb_print: {:d}".format(nb_print))
-nb_print = safe_print_list(my_list, len(my_list) + 2)
-print("nb_print: {:d}".format(nb_print))
+my_square = Square()
+print(type(my_square))
+print(my_square.__dict__)
